60d2e2909206b8d4a4afd5fc69decd44313ec288
vardanreal <vardan.pandya2024@vitstudent.ac.in>
2024-10-01 02:24:58 +0530

    fixed issue#2

--- src/main.c
+++ src/main.c
@@ -16,3 +16,21 @@ int main() {
   start(srv);
   return 0;
 }
+
+
+// Signal handler for SIGINT (Ctrl+C)
+void handle_sigint(int sig) {
+    printf("\nCaught SIGINT (Ctrl+C), shutting down the server...\n");
+    // Assuming there's a function to stop and clean up the server
+    exit(0);
+}
+
+int main() {
+    // Set up SIGINT handler
+    signal(SIGINT, handle_sigint);
+
+    Server *srv = (Server *)MakeServer(AF_INET, PORT, SOCK_STREAM, PROTOCOL,
+                                       BACKLOG, INADDR_ANY);
+    start(srv);
+    return 0;
+}
